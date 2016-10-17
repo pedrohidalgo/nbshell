3b9384177ed2c5a93674adfc0a7d74b65c475e41
Pedro Hidalgo <pedroantoniohidalgo@gmail.com>
2016-10-17 08:00:14 -0400

    #fix #1 Code completion entries are not readable in darcula laf

--- src/main/java/com/qualixium/nbshell/util/AutoCompletionUtil.java
+++ src/main/java/com/qualixium/nbshell/util/AutoCompletionUtil.java
@@ -8,13 +8,15 @@ public class AutoCompletionUtil {
     public static final String LOOK_AND_FEEL_NAME = UIManager.getLookAndFeel().getName();
     public static final Color FONT_COLOR_NORMAL = Color.decode("0x0000B2");
     public static final Color FONT_COLOR_DARK = Color.decode("#46F25A");
-    
+
     public static Color getFontColorToUse(boolean isSelected) {
         Color fontColorToUse;
         if (isSelected) {
             fontColorToUse = Color.WHITE;
         } else {
-            if (LOOK_AND_FEEL_NAME.contains("Dark")) {
+            //TODO verify this to improve the way to deal with Colors on look and feels:
+            //https://netbeans.org/bugzilla/show_bug.cgi?id=257867
+            if (LOOK_AND_FEEL_NAME.contains("Dark") || LOOK_AND_FEEL_NAME.equalsIgnoreCase("Darcula")) {
                 fontColorToUse = FONT_COLOR_DARK;
             } else {
                 fontColorToUse = FONT_COLOR_NORMAL;
